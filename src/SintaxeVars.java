a7ca1f2b1621ced3e1228641d6c083dc8b3f28eb
Alexandregv1080p <alexandregv1999@gmail.com>
2023-02-13 14:31:37 -0300

    Escopo variáveis

---
+++ src/SintaxeVars.java
@@ -0,0 +1,8 @@
+public class SintaxeVars {
+    public static void main(String[] args) {
+        String meuNome = "Alexandre";
+        int anoFabricao = 2022;
+        boolean verdade = false;
+        System.out.println("Nome: " + meuNome + " ano de fabricação: " + anoFabricao + " boolean: " + verdade);
+    }
+}
