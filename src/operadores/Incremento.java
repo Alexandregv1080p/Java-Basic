b08b801aec101483b0c91547140e737edb791e4a
Alexandregv1080p <alexandregv1999@gmail.com>
2023-02-14 10:01:48 -0300

    Operadores

---
+++ src/operadores/Incremento.java
@@ -0,0 +1,15 @@
+package operadores;
+
+public class Incremento {
+    public static void main(String[] args) {
+        int numero = 5;
+
+        //numero = numero + 2;
+        numero+=2;
+
+        System.out.println(numero);
+
+        boolean verd = true;
+        System.out.println(!verd);//inverte o valor do boolean
+    }
+}
--- src/operadores/Operators.java
+++ src/operadores/Operators.java
@@ -1,4 +1,12 @@
 package operadores;
 
 public class Operators {
+    public static void main(String[] args) {
+        int numero = 5;
+        numero =- numero;
+        System.out.println(numero);//negamos o valor
+        numero =+ numero * -1; //convertemos ele para positivo
+        System.out.println(numero);
+    }
+
 }
