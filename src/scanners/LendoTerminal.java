8c58e37dc6d212a023a763862df9084ae21c6f8c
Alexandregv1080p <alexandregv1999@gmail.com>
2023-02-14 19:44:47 -0300

    Terminei Java Basico

--- src/scanners/LendoTerminal.java
+++ src/scanners/LendoTerminal.java
@@ -1,7 +1,21 @@
 package scanners;
 
+import java.util.Scanner;
+
 public class LendoTerminal {
     public static void main(String[] args) {
-        System.out.println("Teste");
+        //configuração que permite o usuario interagir com o terminal
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite seu nome");
+        String nome = scanner.next();//mesma forma q o c#
+
+        System.out.println("Digite sua idade");
+        int idade = scanner.nextInt();
+
+        System.out.println("Digite seu peso");
+        double peso = scanner.nextDouble();
+
+        System.out.println("Nome: "+ nome + " Idade: " + idade +" Peso: "+ peso);
     }
 }
